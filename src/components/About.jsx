897d2b9e16d832cb929943ebff370ace9751821c
Arinze Cyril <arinzecyril123@gmail.com>
2024-01-27 00:48:43 +0100

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import pix1 from '../assets/images/Mario and Adrian A.jpg';
+import pix2 from '../assets/images/Mario and Adrian b.jpg';
+
+const About = () => {
+  return (
+    <section className='abouts'>
+        <div>
+            <div>
+                <h2>Little Lemon</h2>
+                <h3>Nigeria</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis error iste
+                fugiat, laborum dicta nostrum ullam totam quae illum, doloribus iusto beatae
+                natus autem accusantium, sapiente earum in perspiciatis? Corporis.
+                Exercitationem, tenetur quis id eius ut perferendis veniam expedita, architecto,
+                error dolor quasi molestiae labore ipsa natus non? Aspernatur nulla ipsa totam
+                quidem mollitia, odit maiores ad quos provident et.
+            </p>
+        </div>
+        <div className='about-img'>
+            <img src={pix2} alt="" />
+            <img src={pix1} alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default About
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import HeroSection from './HeroSection';
 import DailySpecials from './DailySpecials';
 import Testimonials from './Testimonials';
+import About from './About';
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <HeroSection />
       <DailySpecials />
       <Testimonials />
+      <About />
     </main>
   )
 }
