b9e06c64db83083fce4e03bd06650c648ca2f7bc
Arinze Cyril <arinzecyril123@gmail.com>
2024-06-03 06:25:18 +0100

    fixed a bug on the order online btn in DailySpecial component

--- src/components/DailySpecials.jsx
+++ src/components/DailySpecials.jsx
@@ -5,26 +5,33 @@ import Button from './ReserveTableBTN';
 import {specials} from '../data/allData';
 
 const DailySpecials = () => {
-const dailySpecials = specials.map(card => {
-  return (<>
-    <SpecialCard
-      key={card.id}
-      pix={card.pix}
-      name={card.name}
-      price={card.price}
-      desc={card.desc}
-    />
-  </> )
-})
+  const dailySpecials = specials.map(card => {
+    return (<>
+      <SpecialCard
+        key={card.id}
+        pix={card.pix}
+        name={card.name}
+        price={card.price}
+        desc={card.desc}
+      />
+    </> )
+  })
+  const links = window.location.href.toString()
+  const linkArr = links.split('/')
+  const linkPositionIndex = linkArr.length-1
+  const linkPosition = linkArr[linkPositionIndex]
+  console.log(linkPosition)
   return (
     <section id='menu' className='daily-specials'>
       <div>
         <h2>Specials</h2>
-        <Button text="Order Online" link={'order'} />
+        {linkPosition === "order" ? (
+          <Button text='Order Online'/>
+        ) : (
+          <Button text='Order Online' link={"order"} />
+        )}
       </div>
-      <div className='cards'>
-        {dailySpecials}
-      </div>
+      <div className='cards'>{dailySpecials}</div>
     </section>
   )
 }
